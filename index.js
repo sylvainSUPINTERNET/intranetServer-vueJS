9746f16923e931106a4a4b6f0d026a2b3d0cb602
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2017-10-31 14:02:32 +0100

    suite

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 'use strict';
 
+//use nvm pour switch de node version
+//nvm list
+//nvm use <version>
+
+//nvm install <node_version>
+
+//node -v
+
 require('colors'); //permet d'équiper le type String de toutes plein de méthode (qui permet le display de color)
+
+const util = require('util');
+
+//on peut promise des méthode avec util.promisify()
+//exemple
+//const readFile = util.promisify(fs.readFile) //on peut promissiy tous
+// readFile('filename.txt').then(data => {data.toString()}).catch(console.error)
+
+//async / await
+//async function appStart() { const fileContent = await readFile('filename.txt) }
+
+//les deux font la m^mee chose et permette de "blocker un traitement en attendant une reponse (une promesse)" sans blocker le processus node (dispo depuis v8)
 
 const express = require('express');
 
@@ -13,9 +33,37 @@ const mongoose = require('mongoose');
 
 
 app.get('/', function(req,res){
-    req.send('')
+    res.send('Hello world')
 });
 
 
-app.listen(1337, () => console.log(' Server started ...' .rainbow)
-);
+
+//config via express (creer des clef)
+app.set('ip', 'localhost');
+app.set('port', 1337);
+
+app.set('db_name', 'intranet');
+
+// demare la db au dermarage du server
+//indique a mongoose qu'on utilise les promesses à utiliser sont celle par défaut dans Node.js(objet global)
+mongoose.Promise = global.Promise
+
+// Transformation de la méthode app.listen() d'Express en "Promesse JS"
+const appListen = (app, port, ip) => {
+    return new Promise((resolve, reject) => {
+        app.listen(port, ip, resolve)
+    })
+}
+
+// Connexion à la base de données MONGO,
+
+//attention promissify ici et codé par nous même, car les function qui se promisiffy fonctionne SEULEMENT si elle sont de type retyour err,data et non de retour callback
+//donc uniquement les method convionné node (err,data) en retour se promissify sinon on le fait à la main
+//ainsi listen venat de express ne peut pas etre promissify car il return une err ou une callback
+
+mongoose
+    .connect('mongodb://localhost:27017/intranet', {useMongoClient:true})
+    .then( () => console.log('MongoDB : Connexion établie'.bgGreen) )
+    .then( appListen(app, app.get('port'), app.get('ip')) )
+    .then( () => console.log(` App Started on http://${app.get('ip')}:${app.get('port')} `.bgGreen) )
+    .catch(err => console.log(err.message.red))
